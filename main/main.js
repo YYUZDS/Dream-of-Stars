cb936d95f7d8585e103502407092db4a89059323
yuexiaolingmeng <741144652@qq.com>
2026-01-14 23:12:26 +0800

    bugfix

--- main/main.js
+++ main/main.js
@@ -1,8 +1,8 @@
-import { lib, game, ui, get, ai, _status } from '../../../noname.js'
-import { character } from '../character/index.js'
-import { skill } from '../character/skill.js'
-// import { card } from '../card/card.js'
-import { basic } from './basic.js'
+import { lib, game, ui, get, ai, _status } from "noname";
+import { character } from "../character/index.js"
+import { skill } from "../character/skill.js"
+// import { card } from "../card/card.js"
+import { basic } from "./basic.js"
 export let extensionDefaultPackage = async function () {
     return {
         character: await basic.resolve(character),
@@ -14,7 +14,7 @@ export let extensionDefaultPackage = async function () {
                 "本扩展武将除涉及本扩展装备外（如神武）的武将基本支持单向联机，即客机没有装本扩展也可使用。",
                 "水平有限，如关不掉武将包可以尝试关闭武将包里的小包或者单个武将。",
             ];
-            return '<p style="color:rgb(210,210,000); font-size:12px; line-height:14px; text-shadow: 0 0 2px black;">' + log.join("<br>•") + "</p>";
+            return "<p style='color: rgb(210, 210,000); font - size: 12px; line - height: 14px; text - shadow: 0 0 2px black; '>" + log.join("<br>•") + "</p>";
         })(),
     };
 }
